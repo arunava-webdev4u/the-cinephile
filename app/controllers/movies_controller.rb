1fe3d487f971d4feaf9c947d78730d555d3c5271
Arunava Das <arunava52486@gmail.com>
2025-01-31 01:41:48 +0530

    added new routes, refactored code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
-  def search_by_name
+  def search_movies_by_name
     query = params[:query]
     if query.present?
       tmdb_service = TmdbService.new
@@ -10,6 +10,21 @@ class MoviesController < ApplicationController
       render json: { error: 'Query parameter is missing' }, status: :bad_request
     end
   end
+
+  def search_tv_shows_by_name
+    query = params[:query]
+    if query.present?
+      tmdb_service = TmdbService.new
+      @tv_shows = tmdb_service.search_tv_shows(query)
+      render json: @tv_shows
+    else
+      render json: { error: 'Query parameter is missing' }, status: :bad_request
+    end
+  end
+
+
+
+
 
   def popular
     tmdb_service = TmdbService.new
--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -3,69 +3,82 @@ class TmdbService
     require 'net/http'
     require 'json'
   
-    TMDB_API_KEY = ENV['TMDB_API_KEY'] # Fetch from environment variable for security
-  
+    # https://developer.themoviedb.org/reference/person-popular-list  - TODO
+
+
+
+    # Search by Names
+    def search_tv_shows(query)
+        search_by_name(query, 'tv')
+    end
     def search_movies(query)
-        url = URI("https://api.themoviedb.org/3/search/movie?query=#{query}&include_adult=false&language=en-US&page=1")
-      
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Get.new(url)
-        request["accept"] = 'application/json'
-        request["Authorization"] = 'Bearer ' + ENV['TMDB_API_READ_ACCESS_TOKEN']
-    
-        # Make the request and capture the response
-        response = http.request(request)
-
-        # puts "============"
-        # p response.read_body
-    
-        # Parse the JSON response and return it
-        JSON.parse(response.body)
+        search_by_name(query, 'movie')
     end
 
+
+    # Trendings
+    def trending_movies()
+        get_trending('movie')
+    end
+    def trending_people()
+        get_trending('people')
+    end
+    def trending_tv_shows()
+        get_trending('shows')
+    end
+
+
+    # Movie Lists
     def popular        
         url = URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc")
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Get.new(url)
-        request["accept"] = 'application/json'
-        request["Authorization"] = 'Bearer ' + ENV['TMDB_API_READ_ACCESS_TOKEN']
-    
-        response = http.request(request)
-        JSON.parse(response.body)
+        tmdb_request(url)
     end
-
     def top_rated
         url = URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200")
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Get.new(url)
-        request["accept"] = 'application/json'
-        request["Authorization"] = 'Bearer ' + ENV['TMDB_API_READ_ACCESS_TOKEN']
-    
-        response = http.request(request)
-        JSON.parse(response.body)
+        tmdb_request(url)
     end
-
     def upcoming
         url = URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}")
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Get.new(url)
-        request["accept"] = 'application/json'
-        request["Authorization"] = 'Bearer ' + ENV['TMDB_API_READ_ACCESS_TOKEN']
-    
-        response = http.request(request)
-        JSON.parse(response.body)
+        tmdb_request(url)
     end
-
     def now_playing
         url = URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}")
+        tmdb_request(url)
+    end
+
+
+    # TV Shows Lists
+    def airing_today
+        url = URI("https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1")
+        tmdb_request(url)
+    end
+    def on_the_air
+        url = URI("https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1")
+        tmdb_request(url)
+    end
+    def popular
+        url = URI("https://api.themoviedb.org/3/tv/popular?language=en-US&page=1")
+        tmdb_request(url)
+    end
+    def top_rated
+        url = URI("https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1")
+        tmdb_request(url)
+    end
+
+
+    private
+
+    def search_by_name(query, type)
+        url = URI("https://api.themoviedb.org/3/search/#{type}?query=#{query}&include_adult=false&language=en-US&page=1")
+        tmdb_request(url)
+    end
+
+    def get_trending(type)
+        url = URI("https://api.themoviedb.org/3/trending/#{type}/week?language=en-US")
+        tmdb_request(url)
+    end
+
+    def tmdb_request(url)
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
 
@@ -76,5 +89,4 @@ class TmdbService
         response = http.request(request)
         JSON.parse(response.body)
     end
-  end
-  
+end
