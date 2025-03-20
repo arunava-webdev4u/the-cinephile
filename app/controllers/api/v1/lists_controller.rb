5d5250938df18da8c6c9fa88b2ac5ddfdeb24e43
Arunava Das <arunava52486@gmail.com>
2025-03-21 01:34:32 +0530

    added list show, list, create functionality

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ListsController < Api::V1::BaseController
     def index
-        puts "==================#{user_id}=========="
+        user_id = 1
+
         render json: { message: "Invalid user_id"}, status: :unprocessable_entity unless user_id
 
         @lists = List.where(user_id: user_id)
@@ -11,10 +12,30 @@ class Api::V1::ListsController < Api::V1::BaseController
             render json: @lists.errors, status: :not_found
         end
     end
+
+    def show
+        list_id = params[:id]
+        @list = List.find(list_id)
+        render json: @list, status: :ok
+
+    rescue ActiveRecord::RecordNotFound
+        render json: { message: "List Not Found"}, status: :not_found
+    end
+
+    def create
+        new_list = CustomList.new(permitted_params)
+        new_list.user_id = 1            # for now, create for user_id = 1
+
+        if new_list.save!
+            render json: new_list, status: :created
+        else
+            render json: { message: "Can't Create"}, status: :unprocessable_entity
+        end
+    end
     
     private
 
-    def user_id
-        params.require(:user_id)
+    def permitted_params
+        params.require(:list).permit(:name, :description)
     end
 end
